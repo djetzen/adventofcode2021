08bed421964e2eca89d0884552aef0494d8bade3
DJetzen <djetzen@users.noreply.github.com>
2021-12-10 15:38:52 +0100

    second part

--- src/main/java/de/djetzen/PositionCalculator.java
+++ src/main/java/de/djetzen/PositionCalculator.java
@@ -10,8 +10,8 @@ public class PositionCalculator {
         var point = new Point();
         for (var line : allLinesAsDirection) {
             point = new Point(
-                    getSimpleXValue((int) point.getX(), line),
-                    getSimpleYValue((int) point.getY(), line));
+                    getSimpleXValue(point.x, line),
+                    getSimpleYValue(point.y, line));
         }
 
         return point;
@@ -21,21 +21,39 @@ public class PositionCalculator {
         var aim = 0;
         var allLinesAsDirection = FileReader.getAllLinesAsSimpleDirection(fileName);
         var point = new Point();
-        for (var line : allLinesAsDirection) {
+        for (var tuple : allLinesAsDirection) {
             point = new Point(
-                    getSimpleXValue((int) point.getX(), line),
-                    getSimpleYValue((int) point.getY(), line));
+                    getXValueWithAim(point.x, tuple),
+                    getYValueWithAim(point.y, tuple, aim));
+
+            aim = updateAim(tuple, aim);
         }
 
         return point;
     }
 
-    private Integer getXValueWithAim() {
-        return 0;
+    private Integer getXValueWithAim(Integer previousX, Tuple tuple) {
+        if (tuple.direction() == Direction.FORWARD) {
+            return previousX + tuple.value();
+        }
+        return previousX;
     }
 
-    private Integer getYValueWithAim() {
-        return 0;
+    private Integer getYValueWithAim(Integer previousY, Tuple tuple, int aim) {
+        if (tuple.direction() == Direction.FORWARD) {
+            return previousY + (tuple.value() * aim);
+        }
+        return previousY;
+    }
+
+    private Integer updateAim(Tuple tuple, int aim) {
+        if (tuple.direction() == Direction.DOWN) {
+            return aim + tuple.value();
+        }
+        if (tuple.direction() == Direction.UP) {
+            return aim - tuple.value();
+        }
+        return aim;
     }
 
 
--- src/test/java/de/djetzen/Day2Test.java
+++ src/test/java/de/djetzen/Day2Test.java
@@ -15,7 +15,7 @@ class Day2Test {
 
         Point point = positionCalculator.calculateSimplePosition("src/test/resources/example/day2.txt");
 
-        var solution = point.getX() * point.getY();
+        double solution = point.getX() * point.getY();
 
         assertThat(point.getX()).isEqualTo(15);
         assertThat(point.getY()).isEqualTo(10);
@@ -28,10 +28,39 @@ class Day2Test {
 
         Point point = positionCalculator.calculateSimplePosition("src/test/resources/real/day2.txt");
 
-        var solution = point.getX() * point.getY();
+        double solution = point.getX() * point.getY();
 
         assertThat(point.getX()).isEqualTo(1965);
         assertThat(point.getY()).isEqualTo(1182);
         assertThat(solution).isEqualTo(2322630);
     }
+
+    @Test
+    void the_example_for_the_second_part() throws IOException {
+        var positionCalculator = new PositionCalculator();
+
+        Point point = positionCalculator.calculatePositionWithAim("src/test/resources/example/day2.txt");
+
+        double solution = point.getX() * point.getY();
+
+        assertThat(point.getX()).isEqualTo(15);
+        assertThat(point.getY()).isEqualTo(60);
+        assertThat(solution).isEqualTo(900);
+    }
+
+    @Test
+    void the_real_input_for_the_second_part() throws IOException {
+        var positionCalculator = new PositionCalculator();
+
+        Point point = positionCalculator.calculatePositionWithAim("src/test/resources/real/day2.txt");
+
+        double solution = point.getX() * point.getY();
+
+        assertThat(point.getX()).isEqualTo(1965);
+        assertThat(point.getY()).isEqualTo(1071386);
+        //double
+        assertThat(solution).isEqualTo(2105273490);
+    }
+
+
 }
