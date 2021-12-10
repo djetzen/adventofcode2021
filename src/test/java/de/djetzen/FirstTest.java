f618adbf7588d39bd2dc00529d9c80088691de80
DJetzen <djetzen@users.noreply.github.com>
2021-12-10 08:36:04 +0100

    add dummy test

---
+++ src/test/java/de/djetzen/FirstTest.java
@@ -0,0 +1,13 @@
+package de.djetzen;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FirstTest {
+
+    @Test
+    void test() {
+        assertThat(true).isEqualTo(true);
+    }
+}
